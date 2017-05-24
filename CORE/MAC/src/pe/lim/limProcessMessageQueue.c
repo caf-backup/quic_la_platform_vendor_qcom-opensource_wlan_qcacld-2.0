263812dde980580dd15a5541913a04f1d8e298a1
Abhishek Singh <absingh@codeaurora.org>
2017-05-24 14:50:15 +0530

    qcacld-2.0: Remove unused code for BA request/response
    BA request and response are taken care in FW and thus code in
    host is not used. Thus remove unused code for BA request/response.
    
    Change-Id: Id0056e04eaa8a11c19415c67c2669ea061d77d11
    CRs-Fixed: 2044049

--- CORE/MAC/src/pe/lim/limProcessMessageQueue.c
+++ CORE/MAC/src/pe/lim/limProcessMessageQueue.c
@@ -123,7 +123,6 @@ defMsgDecision(tpAniSirGlobal pMac, tpSirMsgQ  limMsg)
         (limMsg->type != WDA_REMOVE_BSSKEY_RSP) &&
         (limMsg->type != WDA_REMOVE_STAKEY_RSP) &&
         (limMsg->type != WDA_SET_MIMOPS_RSP)&&
-        (limMsg->type != WDA_ADDBA_RSP) &&
         (limMsg->type != WDA_ENTER_BMPS_RSP) &&
         (limMsg->type != WDA_EXIT_BMPS_RSP) &&
         (limMsg->type != WDA_ENTER_IMPS_RSP) &&
@@ -1395,7 +1394,6 @@ limProcessMessages(tpAniSirGlobal pMac, tpSirMsgQ  limMsg)
         case eWNI_SME_REASSOC_CNF:
         case eWNI_SME_ADDTS_REQ:
         case eWNI_SME_DELTS_REQ:
-        case eWNI_SME_DEL_BA_PEER_IND:
         case eWNI_SME_SET_TX_POWER_REQ:
         case eWNI_SME_GET_TX_POWER_REQ:
         case eWNI_SME_GET_NOISE_REQ:
@@ -1680,16 +1678,6 @@ limProcessMessages(tpAniSirGlobal pMac, tpSirMsgQ  limMsg)
         case SIR_LIM_DEL_TS_IND:
           limProcessDelTsInd(pMac, limMsg);
             break;
-        case SIR_LIM_ADD_BA_IND:
-            limProcessAddBaInd(pMac, limMsg);
-            break;
-        case SIR_LIM_DEL_BA_ALL_IND:
-            limDelAllBASessions(pMac);
-            break;
-        case SIR_LIM_DEL_BA_IND:
-            limProcessMlmHalBADeleteInd( pMac, limMsg );
-            break;
-
          case SIR_LIM_BEACON_GEN_IND: {
 
                 if( pMac->lim.gLimSystemRole != eLIM_AP_ROLE )
@@ -1949,9 +1937,6 @@ limProcessMessages(tpAniSirGlobal pMac, tpSirMsgQ  limMsg)
         case WDA_REMOVE_BSSKEY_RSP:
         case WDA_REMOVE_STAKEY_RSP:
             limProcessMlmRemoveKeyRsp( pMac, limMsg );
-            break;
-        case WDA_ADDBA_RSP:
-            limProcessMlmHalAddBARsp( pMac, limMsg );
             break;
 
         case WDA_STA_STAT_RSP:
