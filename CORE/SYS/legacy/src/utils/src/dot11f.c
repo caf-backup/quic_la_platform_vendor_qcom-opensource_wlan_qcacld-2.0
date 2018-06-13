8679555185ffbdb5209e308e69e2355aabe6f529
Qun Zhang <qunz@codeaurora.org>
2018-06-13 11:05:39 +0800

    qcacld-2.0: Add missing frame parser changes
    Part of below two changes info are missed in CLD2.0 latest dotllf.c,
    sync the version between perforce and CLD2.0 main branch.
    I1054c7df18780469849be55fc4343f09ac502a49 (CR2058261)
    If334c66e6b84d9776b0f1f5c1e1969125d490f2f (CR2150438)
    
    Change-Id: I9a023f5920cd4c73998fa40820f60d960c094516
    CRs-Fixed: 2259507

--- CORE/SYS/legacy/src/utils/src/dot11f.c
+++ CORE/SYS/legacy/src/utils/src/dot11f.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2018 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -18704,11 +18704,7 @@ static tANI_U32 UnpackCore(tpAniSirGlobal pCtx,
                         status |= dot11fUnpackIeMobilityDomain(pCtx, pBufRemaining, len, ( tDot11fIEMobilityDomain* )(pFrm + pIe->offset + sizeof(tDot11fIEMobilityDomain)*countOffset) );
                             break;
                 case SigIeNeighborReport:
-                        if (countOffset < MAX_SUPPORTED_NEIGHBOR_RPT) {
-                            status |= dot11fUnpackIeNeighborReport(pCtx, pBufRemaining, len, ( tDot11fIENeighborReport* )(pFrm + pIe->offset + sizeof(tDot11fIENeighborReport)*countOffset) );
-                        } else {
-                            status |= DOT11F_BUFFER_OVERFLOW;
-                        }
+                        status |= dot11fUnpackIeNeighborReport(pCtx, pBufRemaining, len, ( tDot11fIENeighborReport* )(pFrm + pIe->offset + sizeof(tDot11fIENeighborReport)*countOffset) );
                             break;
                 case SigIeOBSSScanParameters:
                         status |= dot11fUnpackIeOBSSScanParameters(pCtx, pBufRemaining, len, ( tDot11fIEOBSSScanParameters* )(pFrm + pIe->offset + sizeof(tDot11fIEOBSSScanParameters)*countOffset) );
@@ -19887,11 +19883,8 @@ tANI_U32 dot11fGetPackedIERSN(tpAniSirGlobal pCtx, tDot11fIERSN *pIe, tANI_U32 *
         }
         else break;
         *pnNeeded += ( pIe->akm_suite_count * 4 );
-        if ( pIe->RSN_Cap )
-        {
-            *pnNeeded += 2;
-        }
-        else break;
+        /* RSN_Cap */
+        *pnNeeded += 2;
         if ( pIe->pmkid_count )
         {
             *pnNeeded += 2;
@@ -20569,7 +20562,7 @@ static tANI_U32 GetPackedSizeCore(tpAniSirGlobal pCtx,
     (void)pCtx; /* Shutup the compiler if we have no FFs nor IEs... */
     i=0; n=0;
     pIe = &( IEs[0] );
-    while ( 0xff != pIe->eid )
+    while (0xff != pIe->eid )
     {
         pfFound = (tFRAMES_BOOL*)(pFrm + pIe->offset +
                                   pIe->presenceOffset);
@@ -27534,12 +27527,10 @@ tANI_U32 dot11fPackIeRSN(tpAniSirGlobal pCtx,
         DOT11F_MEMCPY(pCtx, pBuf, &( pSrc->akm_suites ), ( pSrc->akm_suite_count * 4 ));
         *pnConsumed += ( pSrc->akm_suite_count * 4 );
         pBuf += ( pSrc->akm_suite_count * 4 );
-        if ( pSrc->RSN_Cap )        {
-            DOT11F_MEMCPY(pCtx, pBuf, pSrc->RSN_Cap, 2);
-            *pnConsumed += 2;
-            pBuf += 2;
-        }
-        else break;
+        /* RSN_Cap */
+        DOT11F_MEMCPY(pCtx, pBuf, pSrc->RSN_Cap, 2);
+        *pnConsumed += 2;
+        pBuf += 2;
         if ( pSrc->pmkid_count )        {
             frameshtons(pCtx, pBuf, pSrc->pmkid_count, 0);
             *pnConsumed += 2;
@@ -41219,7 +41210,7 @@ static tANI_U32 PackCore(tpAniSirGlobal pCtx,
     }
 
     pIe = &( IEs[0] );
-    while ( 0xff != pIe->eid )
+    while (0xff != pIe->eid )
     {
         pfFound = (tFRAMES_BOOL*)(pSrc + pIe->offset +
                                   pIe->presenceOffset);
