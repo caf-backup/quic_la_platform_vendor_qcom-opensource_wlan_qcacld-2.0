62c91572237e69f1a6fd0967f26c899959db85d2
Lihua Liu <lihual@codeaurora.org>
2018-06-20 14:29:24 +0800

    qcacld-2.0: Add vdev id sanity check in wma stats event handlers
    Propagation from qcacld-3.0 to qcacld-2.0
    
    Stats events are sent by WLAN FW based on over the air frame reception
    and may contain incorrect vdev id hence sanitize vdev id received from
    FW in stats events before accessing interface array based on it.
    
    Change-Id: I4ecc73fc27285c98c0ea8cebc27955213cd68399
    CRs-Fixed: 2264008

--- CORE/SERVICES/WMA/wma.c
+++ CORE/SERVICES/WMA/wma.c
@@ -1540,6 +1540,11 @@ static void wma_update_vdev_stats(tp_wma_handle wma,
 	v_S7_t rssi = 0;
 	tAniGetRssiReq *pGetRssiReq = (tAniGetRssiReq*)wma->pGetRssiReq;
 
+	if (vdev_stats->vdev_id >= wma->max_bssid) {
+		WMA_LOGE("%s: Invalid vdev_id %hu",
+		 __func__, vdev_stats->vdev_id);
+		return;
+	}
 	node = &wma->interfaces[vdev_stats->vdev_id];
 	stats_rsp_params = node->stats_rsp;
 	if (stats_rsp_params) {
