1c186136e8149e8e6a56c8a841862c80610624d6
bings <bings@codeaurora.org>
2019-02-18 17:05:23 +0800

    qcacld-2.0: Remove the ssidlen check in get_sta_stats
    propagation from qcacld-3.0 to qcacld-2.0
    
    Per protocol definition, the ssid length in OWE bsss IE is zero
    when in OWE transition mode. And the api wlan_hdd_get_sta_stats
    thinks it is invalid ssidlen and returns directly. It causes the
    uplayer can not get stats info.
    
    Change-Id: I20eb7aa79ce25a4b66d9be093510eb414d4fca42
    CRs-Fixed: 2406181

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -12761,7 +12761,6 @@ static int __wlan_hdd_cfg80211_get_station(struct wiphy *wiphy,
 {
     hdd_adapter_t *pAdapter = WLAN_HDD_GET_PRIV_PTR( dev );
     hdd_station_ctx_t *pHddStaCtx = WLAN_HDD_GET_STATION_CTX_PTR(pAdapter);
-    int ssidlen = pHddStaCtx->conn_info.SSID.SSID.length;
     tANI_U8 rate_flags;
 
     hdd_context_t *pHddCtx = (hdd_context_t*) wiphy_priv(wiphy);
@@ -12792,11 +12791,9 @@ static int __wlan_hdd_cfg80211_get_station(struct wiphy *wiphy,
 
     ENTER();
 
-    if ((eConnectionState_Associated != pHddStaCtx->conn_info.connState) ||
-            (0 == ssidlen))
+    if (eConnectionState_Associated != pHddStaCtx->conn_info.connState)
     {
-        hddLog(VOS_TRACE_LEVEL_INFO, "%s: Not associated or"
-                    " Invalid ssidlen, %d", __func__, ssidlen);
+        hddLog(VOS_TRACE_LEVEL_INFO, "%s: Not associated", __func__);
         /*To keep GUI happy*/
         return 0;
     }
