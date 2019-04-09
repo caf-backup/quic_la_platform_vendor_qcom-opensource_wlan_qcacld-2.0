a081bbf14a7aa1e00e506e616eb500c1b9b5bd78
gaolez <gaolez@codeaurora.org>
2019-04-09 14:37:02 +0800

    qcacld-2.0: Add rx time statistic for dump survey
    Add rx time statistic for cfg80211 dump survey interface function.
    
    Change-Id: I3fa4046ca7657f92fe8fa5c13d72efec065afbcb

--- CORE/MAC/inc/sirApi.h
+++ CORE/MAC/inc/sirApi.h
@@ -8579,6 +8579,7 @@ struct sme_flush_pending {
  * @cycle_count: cycle count
  * @rx_clear_count: rx clear count
  * @tx_frame_count: TX frame count
+ * @rx_frame_count: RX frame count
  * @clock_freq: clock frequence MHZ
  */
 struct scan_chan_info {
@@ -8588,6 +8589,7 @@ struct scan_chan_info {
 	uint32_t cycle_count;
 	uint32_t rx_clear_count;
 	uint32_t tx_frame_count;
+	uint32_t rx_frame_count;
 	uint32_t clock_freq;
 };
 
