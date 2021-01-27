8e1e6c7005d07a48a1331a104ba58a1afaa57a70
Wu Gao <wugao@codeaurora.org>
2021-01-27 16:07:38 +0800

    qcacld-2.0: Add IPA support based on CONFIG_IUPA3
    Add IPA support based on CONFIG_IPA3 for SDX12.
    
    Change-Id: I1f3c596e64fca8a5ad1fd370dad2135c7a16b040
    CRs-Fixed: 2865308

--- CORE/HDD/src/wlan_hdd_ipa.c
+++ CORE/HDD/src/wlan_hdd_ipa.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2018 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2018, 2021 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -1903,7 +1903,7 @@ static void hdd_ipa_uc_op_cb(struct op_msg_type *op_msg, void *usr_ctxt)
 			ipa_stat.tx_ch_stats.num_db,
 			ipa_stat.tx_ch_stats.num_unexpected_db,
 			ipa_stat.tx_ch_stats.num_bam_int_handled,
-			ipa_stat.tx_ch_stats.num_bam_int_in_non_runnning_state,
+			ipa_stat.tx_ch_stats.num_bam_int_in_non_running_state,
 			ipa_stat.tx_ch_stats.num_qmb_int_handled);
 
 		VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
