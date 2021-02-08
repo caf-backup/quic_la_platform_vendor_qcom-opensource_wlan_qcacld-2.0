414060d46a4092cd594f1163f3eb4e05a4e0980f
Tiger Yu <tfyu@codeaurora.org>
2021-02-08 14:58:31 +0800

    qcacld-2.0: Flush RX packets for peer after key installation
    Rx packets including fragments are not flushed as part of rekey which
    could result in fragments encrypted under different keys to be
    reassembled.
    
    Fix is to flush all rx pakcets including fragments for the peer after
    key installation.
    
    Change-Id: I80b680040ceab687d0e5ee01c21582d8873a773b
    CRs-Fixed: 2867975

--- CORE/SERVICES/WMA/wma.c
+++ CORE/SERVICES/WMA/wma.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2015, 2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2015, 2019, 2021 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -105,6 +105,8 @@
 
 #include "dfs.h"
 #include "radar_filters.h"
+#include "ol_rx_reorder.h"
+
 /* ################### defines ################### */
 /*
  * TODO: Following constant should be shared by firwmare in
@@ -14893,6 +14895,12 @@ static void wma_set_stakey(tp_wma_handle wma_handle, tpSetStaKeyParams key_info)
 			goto out;
 		}
 	}
+
+        WMA_LOGD("%s: QSV2020002, vid(%u), rx cleanup for peer(%pM) after key install",
+            __func__,
+            txrx_vdev->vdev_id,
+            peer->mac_addr.raw);
+        ol_rx_reorder_peer_cleanup(txrx_vdev, peer);
 
         /* In IBSS mode, set the BSS KEY for this peer
          ** BSS key is supposed to be cache into wma_handle
