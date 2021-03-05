f3b66df936b90d3ec38e18a086b0fd61ab21143d
Tiger Yu <tfyu@codeaurora.org>
2021-03-05 11:16:51 +0800

    qcacld-2.0: Add eapol sanity checking for intra-bss forwarding & indicate
    Dropping all eapol frames during intra-bss forwarding. And add eapol
    sanity checking when indicate the RX pakcets to the up layer.
    
    Change-Id: I7384cb7ae8a6806df7cfe2e25e62d2fa884b2f65
    CRs-Fixed: 2868053

--- CORE/HDD/inc/wlan_hdd_cfg.h
+++ CORE/HDD/inc/wlan_hdd_cfg.h
@@ -2900,6 +2900,18 @@ This feature requires the dependent cfg.ini "gRoamPrefer5GHz" set to 1 */
 #define CFG_ENABLE_DEBUG_CONNECT_ISSUE_DEFAULT     (0x76)
 
 /*
+ * Eapol sanity checking Enable/Disable for SAP.
+ * 0x0 - Enable eapol sanity checking in the RX path
+ *       before deliver to the OS in the SAP mode.
+ * 0x1 - Enable eapol sanity checking in the RX path
+ *       before deliver to the OS in the SAP mode.
+ */
+#define CFG_ENABLE_SAP_EAPOL_CHECKING             "gEnableSapEapolChecking"
+#define CFG_ENABLE_SAP_EAPOL_CHECKING_MIN         (0)
+#define CFG_ENABLE_SAP_EAPOL_CHECKING_MAX         (1)
+#define CFG_ENABLE_SAP_EAPOL_CHECKING_DEFAULT     (1)
+
+/*
  * RX packet handling options
  * 0: no rx thread, no RPS, for MDM
  * 1: RX thread
@@ -5567,6 +5579,7 @@ struct hdd_config {
    v_U32_t                     TxPower2g;
    v_U32_t                     TxPower5g;
    v_U32_t                     gEnableDebugLog;
+   v_U32_t                     gEnableSapEapolChecking;
    uint8_t                     enable_ac_txq_optimize;
    v_U8_t                      rxhandle;
    uint8_t                     cpu_map_list[CFG_RPS_RX_QUEUE_CPU_MAP_LIST_LEN];
